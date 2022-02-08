910da7a68c872bc3433477b5e84d290cc087c417
Maxwell Au <MaxwellA@beckhoff.com>
2022-02-08 21:31:12 +0800

    created machine learning and picking capabilities

---
+++ TcHmiProject2/FunctionJS1.js
@@ -0,0 +1,15 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../Packages/Beckhoff.TwinCAT.HMI.Framework.12.750.1/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var TcHmiProject2;
+        (function (TcHmiProject2) {
+            function FunctionJS1(combobox) {
+                console.log(combobox.getSelectedText());
+            }
+            TcHmiProject2.FunctionJS1 = FunctionJS1;
+        })(TcHmiProject2 = Functions.TcHmiProject2 || (Functions.TcHmiProject2 = {}));
+        Functions.registerFunctionEx('FunctionJS1', 'TcHmi.Functions.TcHmiProject2', TcHmiProject2.FunctionJS1);
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
